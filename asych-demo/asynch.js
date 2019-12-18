9c5b15923747e17391b7eb138ef463f3390eb47e
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2019-12-17 21:19:18 -0500

    complete callbacks

--- asych-demo/asynch.js
+++ asych-demo/asynch.js
@@ -1,12 +1,22 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1,(user) => {
+    // console.log('User', user)
+    getRepositories(user.gitHubUsername, (repos) => {
+    console.log('Repos', repos);
+    });
+});
 console.log('After');
 
-function getUser(id) {
+function getUser(id, callback) {
     setTimeout(() => {
     console.log('Reading a user from a database...');
-    return { id: id, gitHubUsername: 'mosh'};
+    callback({ id: id, gitHubUsername: 'mosh'});
     }, 2000);
-    return 1;
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+       console.log('Calling GitHub API...');
+       callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
 }
