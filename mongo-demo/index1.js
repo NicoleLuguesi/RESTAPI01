f45bf571f8304b38f5b1f3e9bf08f5046db4ee27
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-01-01 16:09:12 -0500

    Val Errors

--- mongo-demo/index1.js
+++ mongo-demo/index1.js
@@ -48,9 +48,9 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
 const course = new Course({
     name: 'Angular Course',
-    category: 'web',
+    category: '-',
     author: 'Mosh',
-   // tags: [], 
+   tags: null, 
     isPublished: true,
     price: 15
 });
@@ -60,8 +60,9 @@ try {
     console.log(result);
 }
 catch (ex) {
-    console.log(ex.message);
-}
+    for (field in ex.errors)
+        console.log(ex.errors[field].message);
+    }
 }
 
 
