1d1f2f81cc3c19ee49c67361360bd462552f53d5
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-01-01 15:45:43 -0500

    custom complete

--- mongo-demo/index1.js
+++ mongo-demo/index1.js
@@ -20,7 +20,15 @@ const courseSchema = new mongoose.Schema({
 
 
     author: String,
-    tags: [ String ], 
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least one tag.'
+        }
+    },
     date: { type: Date, default: Date.now }, 
     isPublished: Boolean, 
     price: {
@@ -36,9 +44,9 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
 const course = new Course({
     name: 'Angular Course',
-    category: '-',
+    category: 'web',
     author: 'Mosh',
-    tags: ['angular', 'frontend'], 
+   // tags: [], 
     isPublished: true,
     price: 15
 });
